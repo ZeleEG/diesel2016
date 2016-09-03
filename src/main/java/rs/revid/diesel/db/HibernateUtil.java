661eb4efe2333e22162b3a679fac4b710922addf
Dejan Pejcev <Dejan@DESKTOP-KJUG7CR>
2016-09-03 17:19:15 +0200

    Initial JSF project setup

---
+++ src/main/java/rs/revid/diesel/db/HibernateUtil.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.revid.diesel.db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Dejan
+ */
+public class HibernateUtil {
+  
+    private static final SessionFactory sessionFactory=buildSessionFactory();
+  
+   public static SessionFactory buildSessionFactory(){
+       SessionFactory factory = null;
+        try {            
+            Configuration cfg = new Configuration();
+            cfg.configure();
+            factory = cfg.buildSessionFactory();
+            /*
+            Configuration configuration = new Configuration().configure();
+	        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+	                .applySettings(configuration.getProperties());
+	        factory = configuration
+	                .buildSessionFactory(builder.build());
+            */
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        return factory;
+    }
+  
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
