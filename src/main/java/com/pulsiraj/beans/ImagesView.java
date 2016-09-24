453ea6ec0972be51a962bf361074b7265cdde0e0
IgorMijatovic94 <igor.mijatovic94@gmail.com>
2016-09-24 23:42:33 +0200

    Reuploading photos because of the size mistake.

---
+++ src/main/java/com/pulsiraj/beans/ImagesView.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pulsiraj.beans;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Igor.Mijatovic
+ */
+
+@ManagedBean
+public class ImagesView {
+
+    private List<String> images;
+
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            images.add("Slika" + i + ".jpg");
+        }
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
